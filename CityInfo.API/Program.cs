1371f01668b2a8ad4d71317c270c38a81bfb816b
lthanh <trgthanh258@gmail.com>
2023-03-22 15:18:50 +0700

    fix

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -77,11 +77,13 @@ builder.Services.AddApiVersioning(setupAction =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
