a1a4bf0ffa72b107133a73290721ea83deb1b13e
lthanh <trgthanh258@gmail.com>
2023-03-23 14:45:10 +0700

    eag

--- main.tf
+++ main.tf
@@ -41,22 +41,20 @@ resource "azurerm_app_service" "example" {
   resource_group_name = azurerm_resource_group.example.name
   app_service_plan_id = azurerm_app_service_plan.example.id
 
-  source_control {
-    repo_url               = "https://github.com/trgthanh258/CityInfo/"
-    branch                 = "master"
-  }
-
   site_config {
-    dotnet_framework_version = "v6.0"
-    vnet_route_all_enabled   = true
-    scm_type = "GitHub"
+    scm_type = "ExternalGit"
+  }
+
+  source_control {
+    repo_url = "https://github.com/Azure-Samples/app-service-web-html-get-started"
+    branch   = "master"  
   }
 }
 
-resource "azurerm_app_service_source_control_token" "example" {
-  type  = "GitHub"
-  token = "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"
-}
+# resource "azurerm_app_service_source_control_token" "example" {
+#   type  = "GitHub"
+#   token = "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"
+# }
 
 # resource "azurerm_api_management" "example" {
 #   name                = "terra-api-management"
