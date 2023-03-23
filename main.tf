3587623e0aa161f7ce562f100de834d5e270921b
lthanh <trgthanh258@gmail.com>
2023-03-23 10:47:53 +0700

    Terraform - Create Resource group, App service plan and App service

--- main.tf
+++ main.tf
@@ -25,37 +25,31 @@ resource "azurerm_resource_group" "example" {
   location = "southeastasia"
 }
 
-# resource "azurerm_app_service_plan" "example" {
-#   name                = "terra-app-service-plan"
-#   location            = azurerm_resource_group.example.location
-#   resource_group_name = azurerm_resource_group.example.name
-#   sku {
-#     tier = "Standard"
-#     size = "S1"
-#   }
-# }
+resource "azurerm_app_service_plan" "example" {
+  name                = "terra-app-service-plan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
 
-# resource "azurerm_app_service" "example" {
-#   name                = "terra-appservice"
-#   location            = azurerm_resource_group.example.location
-#   resource_group_name = azurerm_resource_group.example.name
-#   app_service_plan_id = azurerm_app_service_plan.example.id
+resource "azurerm_app_service" "example" {
+  name                = "terra-appservice"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
 
-#   site_config {
-#     dotnet_framework_version = "v6.0"
-#     vnet_route_all_enabled   = true
-#   }
+  site_config {
+    dotnet_framework_version = "v6.0"
+  }
 
-#   app_settings = {
-#     WEBSITE_RUN_FROM_PACKAGE       = "1"
-#     SCM_DO_BUILD_DURING_DEPLOYMENT = "true"
-#   }
-
-#   source_control {
-#     repo_url = "https://github.com/trgthanh258/CityInfo/"
-#     branch   = "master"
-#   }
-# }
+  source_control {
+    repo_url = "https://github.com/trgthanh258/CityInfo/"
+    branch   = "master"
+  }
+}
 
 # resource "azurerm_api_management" "example" {
 #   name                = "terra-api-management"
