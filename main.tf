40b6460feb799a4d5d31056dc9d5727d32fe49c9
lthanh <trgthanh258@gmail.com>
2023-03-23 11:22:12 +0700

    Terraform - Create App service

--- main.tf
+++ main.tf
@@ -51,7 +51,6 @@ resource "azurerm_app_service_source_control" "example" {
   app_id                 = azurerm_app_service.example.id
   repo_url               = "https://github.com/trgthanh258/CityInfo/"
   branch                 = "master"
-  uses_github_action     = true
   github_action_configuration {
     generate_workflow_file = false
   }
