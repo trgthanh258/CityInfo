73df5e224cfbd882607118c1dc2f1dc0970863fc
lthanh <trgthanh258@gmail.com>
2023-03-25 12:45:42 +0700

    csacsac

--- main.tf
+++ main.tf
@@ -113,8 +113,7 @@ resource "azurerm_api_management_api_operation_policy" "example" {
         <base />
       </inbound>
       <backend>
-        <set-backend-service base-url="${azurerm_api_management_backend.example.url}" />
-        <rewrite-uri template="/api/cities" />
+        <set-backend-service base-url="${azurerm_api_management_backend.example.url}/api/cities" />
       </backend>
       <outbound>
         <base />
